6ff58d12f5deadb7d69804df1254fa2504ea51c9
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-11 17:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function whisper(string) {
   console.log(string)
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
